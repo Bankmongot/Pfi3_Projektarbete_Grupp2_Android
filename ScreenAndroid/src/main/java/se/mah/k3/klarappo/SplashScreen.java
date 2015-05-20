b6d2e6072b212da747ec26ecd80372446ead32fc
Therealnaxos <nils.lindgren.92@gmail.com>
2015-05-20 14:32:55 +0200

    asdad

---
+++ ScreenAndroid/src/main/java/se/mah/k3/klarappo/SplashScreen.java
@@ -0,0 +1,91 @@
+package se.mah.k3.klarappo;
+
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SplashScreen extends Fragment {
+
+    private boolean timeToRun;
+    private Firebase myFirebaseRef;
+
+
+
+
+    public SplashScreen() {
+        // Required empty public constructor
+    }
+
+    public void checkRun() {
+        myFirebaseRef = new Firebase("https://popping-torch-1741.firebaseio.com/Active");
+
+
+
+        myFirebaseRef.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                //timeToRun = snapshot.getValue();
+                System.out.println(snapshot.getValue());
+                timeToRun = (boolean) snapshot.getValue();
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+
+        });
+
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        checkRun();
+
+        if(timeToRun == false){
+            Log.d("SplashScreen", "false, expanding LoginFragment!!");
+
+
+            FragmentManager fm;
+            fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.container, new LoginFragment());
+            ft.commit();
+        } else if ( timeToRun == true){
+            FragmentManager fm;
+            fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.container, new MainFragment());
+            ft.commit();
+        }
+
+
+
+
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false);
+    }
+
+
+}
