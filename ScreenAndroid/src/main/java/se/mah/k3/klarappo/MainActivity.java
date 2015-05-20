85d71118cc2044db44c7fadfae38c25266fd61f7
Therealnaxos <nils.lindgren.92@gmail.com>
2015-05-20 14:22:48 +0200

    ohhhh yeaaa

--- ScreenAndroid/src/main/java/se/mah/k3/klarappo/MainActivity.java
+++ ScreenAndroid/src/main/java/se/mah/k3/klarappo/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends Activity {
             FragmentManager fm;
             fm = getFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
-            ft.replace(R.id.container, new LoginFragment());
+            ft.replace(R.id.container, new SplashScreen());
             ft.commit();
         }
     }
